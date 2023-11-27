791499108d8d0961b21818832126763bfc5404e1
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2023-11-27 16:00:19 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,6 +20,10 @@
     <script type="module" src="index.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
 
 <link href="https://fonts.googleapis.com/css2?family=Grechen+Fuemen&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" rel="stylesheet">
